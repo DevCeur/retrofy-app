5eb955b2def351eaa6060e96bccdb9d629f6b467
Carlos Umaña <dev.carlos.umana@gmail.com>
2023-03-09 08:31:23 -0500

    [initial]: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Retrofy | Make your Retro meeting, more cool.
---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+const HomeRoute = () => {
+  return (
+    <div>
+      <h1>Retrofy</h1>
+    </div>
+  );
+};
+
+export default HomeRoute;
